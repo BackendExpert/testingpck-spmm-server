775653c6fe34fd392cefb0b3f9afff012c4db2ca
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-03 12:15:59 +0530

    Update

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,5 +1,43 @@
-const AuthController = {
+const User = require("../Model/User");
+const bcrypt = require('bcrypt');
 
+const AuthController = {
+    signup: async(req, res) => {
+        try{
+            const {
+                username,
+                email,
+                password
+            } = req.body
+
+            const checkUser = await User.findOne(
+                {
+                    $or: [
+                        { username: username },
+                        { email: email }
+                    ]
+                }
+            )
+
+            if(checkUser){
+                return res.josn({ Error: 'User Already exists....'})
+            }
+
+            const hashPass = await bcrypt.hash(password, 10);
+
+            const newUser = new User({
+                username: username,
+                email: email,
+                password: hashPass,                
+            })
+
+            const resutlUser = await newUser.save()
+            return res.json({Status: "Success"})
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
 };
 
 module.exports = AuthController;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const ConnectDB = require('./Config/DB');
 
+// routes init
+const AuthRoute = require('./Route/AuthRoute')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+
+app.use('/auth', AuthRoute)
 
 
 app.get('/', (req, res) => {
